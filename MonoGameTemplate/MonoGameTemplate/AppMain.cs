ed8269053fe4c2cc774957dede7c787c9d2be531
Kenji Shimojima <shanageek@gmail.com>
2013-11-20 22:18:49 +0900

    MonoGamesの雛形を追加。

---
+++ MonoGameTemplate/MonoGameTemplate/AppMain.cs
@@ -0,0 +1,27 @@
+using System;
+using System.Collections.Generic;
+
+using Sce.PlayStation.Core;
+using Sce.PlayStation.Core.Environment;
+using Sce.PlayStation.Core.Graphics;
+using Sce.PlayStation.Core.Input;
+
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using Microsoft.Xna.Framework.Storage;
+using Microsoft.Xna.Framework.GamerServices;
+
+namespace MonoGameTemplate
+{
+	public class AppMain
+	{
+		public static void Main (string[] args)
+		{
+            using (var game = new Game1())
+                game.Run();
+		}
+	}
+}
+
